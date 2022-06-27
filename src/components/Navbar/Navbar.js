78968ee675d71c9bf97aeb3729a28357d9ecf543
Manuel-Argueta <manuel.argueta0314@gmail.com>
2022-06-26 23:00:06 -0700

    added nav links

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,33 @@
+import React from 'react';
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import "./Navbar.css";
+import { NavLink } from "react-router-dom";
 export default function Navbar() {
-    
+
     return (
-    <div>
-        <h1>Nav Here</h1>
-    </div>
+        <div>
+            <Row>
+                <Col>
+                    <p>CSAP</p>
+                </Col>
+                <Col>
+                    <NavLink to="/">
+                        Home
+                    </NavLink>
+                    <NavLink to="/about">
+                        About
+                    </NavLink>
+                    <NavLink to="/contact">
+                        Contact
+                    </NavLink>
+                    <NavLink to="/register">
+                        Register
+                    </NavLink>
+                </Col>
+            </Row>
+
+        </div>
     )
 }

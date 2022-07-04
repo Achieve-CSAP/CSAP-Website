9c912710eeaf6fc945db58169bda1fc9a22ba1f8
Manuel-Argueta <manuel.argueta0314@gmail.com>
2022-07-03 20:29:58 -0700

    home updates

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,30 @@
+import React from "react";
+import './Home.css'
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import kevinHeadshot from "../../assets/kevinHeadshot.png"
+
 export default function Home() {
     
     return (
     <div>
-        <h1>Home</h1>
+        <div id = "heroSection">
+        </div>
+        <Row>
+            <Col>
+            <img src = {kevinHeadshot}/>
+            </Col>
+            <Col>
+            <div>
+                <h1>Experiential learning, hands-on experience,
+                     and civic engagement.</h1>
+                <p>We are a nonprofit with a mission to expose 
+                    high school students in underserved communities 
+                    to the field of Computer Science.</p>
+                <button>Learn More</button>
+            </div>
+            </Col>
+        </Row>
     </div>
     )
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,30 +4,29 @@ import Col from "react-bootstrap/Col";
 
 import "./Navbar.css";
 import { NavLink } from "react-router-dom";
-export default function Navbar() {
 
+export default function Navbar() {
     return (
-        <div>
+        <div id = "navContainer">
             <Row>
                 <Col>
-                    <p>CSAP</p>
+                    <p id = "navLogo">Computer Science Acheivement Program</p>
                 </Col>
                 <Col>
-                    <NavLink to="/">
+                    <NavLink to="/" className="navItem">
                         Home
                     </NavLink>
-                    <NavLink to="/about">
+                    <NavLink to="/about" className="navItem">
                         About
                     </NavLink>
-                    <NavLink to="/contact">
+                    <NavLink to="/contact" className="navItem">
                         Contact
                     </NavLink>
-                    <NavLink to="/register">
+                    <NavLink to="/register" className="navItem">
                         Register
                     </NavLink>
                 </Col>
             </Row>
-
         </div>
     )
 }
